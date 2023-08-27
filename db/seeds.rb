0875eaf521943801ee020fd5322a6e44b83972ea
Hadrien G <hgrach@gmail.com>
2023-08-27 13:49:06 +0200

    update views/bookmarks/_modal V2

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 # require "faker"
-require "JSON"
+require "json"
+require 'uri'
 
 puts "Cleaning database..."
 Movie.destroy_all
